934094b314f82d0a1d7d1b0114ef77d9f485d32b
Archit_Kumar1 <architkumar85@gmail.com>
2019-11-10 02:02:57 +0530

    new

---
+++ Hackerearth/Contests/FightARena/geo.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#pragma optimize("O3","-ofast")
+using namespace std;
+template<class T> ostream& operator<<(ostream &os,vector<T> V){
+    os<<"[ ";for(auto v:V)os<<v<<" ";return os<<"]";
+}
+template<class L,class R> ostream& operator<<(ostream &os,pair<L,R> P){
+    return os<<"("<<P.first<<","<<P.second<<")";
+}
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__,__VA_ARGS__)
+template<typename Arg1>
+void __f(const char* name,Arg1&& arg1){
+	cout<<name<<" : "<<arg1<<endl;
+}
+template<typename Arg1,typename... Args>
+void __f(const char* names,Arg1&& arg1,Args&&... args){
+		const char* comma=strchr(names+1,',');cout.write(names,comma-names)<<" : "<<arg1<<" | ";__f(comma+1,args...);
+}
+#else
+#define trace(...) 1
+#endif
+#define ALL(x) x.begin(),x.end()
+#define LL long long int
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+#define endl "\n"
+#define l() cout << endl;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define TC int t; cin >> t;while(t--)
+#define forn(i,n) for(int i=0;i<n;i++)
+
+const int mod = pow(10,9)+7;
+const double inf = 1e18;
+const LL linf = 2e18;
+const double eps = 1e-12;
+//////////////////////////////////////////////
+
+
+struct point{
+    double x,y;
+};
+struct line {
+    double a,b,c;
+    line(point x,point y){
+        a = y.y - x.y;
+        b = x.x - y.x;
+        c = a*x.x + b*x.y;
+    }
+    double dist(point p){
+        return abs(1.0*(a*p.x  + b*p.y + c)/sqrt(a*a + b*b));
+    }
+    point getp(point p){
+        double temp = -1*(a*p.x  + b*p.y + c)/(a*a + b*b);
+        double x = temp * a + p.x; 
+        double y = temp * b + p.y; 
+        return {x,y}; 
+    }
+};
+int main(){
+    
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif   
+cout.precision(10);cout << fixed;
+    int n;
+    cin >> n;
+    point init;
+    cin >> init.x >> init.y;
+    vector<point > all(n);
+    for(int i=0;i<n;i++){
+        cin >> all[i].x >> all[i].y;
+    }
+    double fans = 1e18;
+    point fpoint;
+
+    for(int i=0;i<n;i++){
+        line l = line(all[i],all[(i+1)%n]);
+        double dd = l.dist(init);
+        if(dd < fans ){
+            fans = dd;
+            fpoint = l.getp(init);
+        }
+    }
+    cout << fpoint.x << " " << fpoint.y << endl;
+}
---
+++ Hackerearth/Contests/FightARena/qeqe.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#pragma optimize("O3","-ofast")
+using namespace std;
+template<class T> ostream& operator<<(ostream &os,vector<T> V){
+    os<<"[ ";for(auto v:V)os<<v<<" ";return os<<"]";
+}
+template<class L,class R> ostream& operator<<(ostream &os,pair<L,R> P){
+    return os<<"("<<P.first<<","<<P.second<<")";
+}
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__,__VA_ARGS__)
+template<typename Arg1>
+void __f(const char* name,Arg1&& arg1){
+	cout<<name<<" : "<<arg1<<endl;
+}
+template<typename Arg1,typename... Args>
+void __f(const char* names,Arg1&& arg1,Args&&... args){
+		const char* comma=strchr(names+1,',');cout.write(names,comma-names)<<" : "<<arg1<<" | ";__f(comma+1,args...);
+}
+#else
+#define trace(...) 1
+#endif
+#define ALL(x) x.begin(),x.end()
+#define LL long long int
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+#define endl "\n"
+#define l() cout << endl;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define TC int t; cin >> t;while(t--)
+#define forn(i,n) for(int i=0;i<n;i++)
+
+const int mod = pow(10,9)+7;
+const double inf = 1e18;
+const LL linf = 2e18;
+const double eps = 1e-12;
+//////////////////////////////////////////////
+
+
+
+int main(){
+    
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif   
+    
+    int n,w;
+    cin >> n >> w;
+    int dp[n+1][w+1];
+    memset(dp,0,sizeof(dp));
+    int wei[n];
+    int val[n];
+    forn(i,n) cin >> val[i];
+    forn(j,n) cin >> wei[j];
+    
+    for(int i=1;i<=n;i++){
+        for(int j = 1;j<=w+;j++){
+            if(j - wei[i-1] >=0)
+                 dp[i][j] = max(dp[i-1][j], dp[i-1][j - wei[i-1]]  + val[i-1]);
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    cout << dp[n][w] << endl;
+    
+}
